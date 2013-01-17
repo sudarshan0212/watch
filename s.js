b62ee7589bf119b2b65c92413982802fe772c2df
sudarshan0212 <sudrn0212@gmail.com>
2013-01-17 16:47:37 +0530

    commited

--- s.js
+++ s.js
@@ -1,159 +1,96 @@
-function validate(e)
+function blank()
 {
-var key=e.which;
-if(key>96&&key<123||key>64&&key<91)
-{
-	return true;
-}
-else
-{
-	return false;
-}
-}
-function pvalidate(e)
-{
-var key=e.which;
-if(key>96&&key<123||key>64&&key<91||key>47&&key<58)
-{
-	return true;
-}
-else
-{
-	return false;
-}
-}
-function cvalidate(e)
-{
-var key=e.which;
-if(key>47&&key<58)
-{
-	return true;
-}
-else
-{
-	return false;
-}
-}
-
-$(document).ready(function(){
-//$('#toggle').click(function() {
-
-$('input:text').focusin(function(){
-var  x=$(this).attr('id');
-   
-   switch(x)
-   {
-	    case '1':
-	       $('#d1').hide();
-		   break;
-        case '2':
-	   	    $('#d2').hide();
-		    break;
-		case '3':
-		   $('#d3').hide();
-		   break;
-		case '7':
-		   $('#d7').hide();
-		   break;
-		  
-   }
-   
-	
-});
-
-$('input:text').focusout(function() {
-	var y=$(this).attr('id');
-
-	switch(y)
+var fname=$('#1').val();
+var lname=$('#2').val();
+var email=$('#3').val();
+var pass=$('#4').val();
+var cpass=$('#5').val();
+var contact=$('#6').val();
+    if(fname==''||lname==''||email==''||pass==''||cpass==''||contact=='')
 	{
-		case '1':
-			if($(this).val()=="")
-			{
-				$('#d1').show();
-			}
-				
-		break;
-	 
-		case '2':
-			if($(this).val()=="")
-			{
-				$('#d2').show();
-			}
-		break;
-
-		case '3':
-			if($(this).val()=="")
-			{
-				$('#d3').show();
-			}
-		break;
-	 
-		case '7':
-			if($(this).val()=="")
-			{
-				$('#d7').show();
-			}
-		break;
-
+			return false;
 	}
-});
-
-$('input:password').focusin(function(){
-var a=$(this).attr('id');
-
-	switch(a)
-    {
-			case '4':
-				$('#d4').hide();
-				break;
-
-			case '5':
-				$('#d5').hide();
-				break;
-    }
-});
-
-$('input:password').focusout(function() {
-var b=$(this).attr('id');
+	else
+	{
+		return true;
+	}
 	
-		switch(b)
-		{
-			case '4':
-				if($(this).val()=="")
-				{
-					$('#d4').show();
-				}
-			break;
+}
+ 
+ function checkForm()
+{
+//var char1 = /^[A-Z]+\.?[A-Z]+$/;
+var char1=/^[a-zA-Z]+$/ ;//characters
+var char2 = /^([a-zA-Z0-9_-]){8,15}$/;//alpanumeric
+var char3 = /^[0-9]+$/ ;//numbers
+var email=/^[A-Z0-9._%+-]+@(?:[A-Z0-9-]+\.)+[A-Z]{2,4}$/;
+var count=0;
+if(blank())
+{
+	if(!char1.test($('#1').val()))
+	{
+			alert("First name should contain only characters");
+			return false;
+	}
+	else
+	{
+		
+	}
+	if(!char1.test($('#2').val()))
+	{
+			alert("Last name should contain only characters");
+			return false;
+	}
+	else
+	{
+		count++;
+	}
+  /* if(!email.test($('#3').val()))
+	{
+		alert("Please enter valid email address");
+		return false;
+	}
+	else
+	{
+		count++;
+	}*/
 
-			case '5':
-				if($(this).val()=="")
-				{
-					$('#d5').show();
-				}
-			break;	
-		}
-});
-
-
-$('#1').keypress(function(e){
-return validate(e);
-});
-$('#2').keypress(function(e){
-return validate(e);
-});
-$('#3').keypress(function(e){
-return validate(e);
-});
-$('#4').keypress(function(e){
-return pvalidate(e);
-});
-$('#5').keypress(function(e){
-return pvalidate(e);
-});
-
-$('#7').keypress(function(e){
-return cvalidate(e);
-});
-
-});
-
+	if(!char2.test($('#4').val())&&!char2.test($('#5').val()))
+	{
+			alert("Password must be alphanumeric and 8-15 in length");
+			return false;
+	}
+	else if($('#4').val()!=$('#5').val())
+	{
+            		alert("Password and confirm password must be same");
+					return false;
+	}
+	else
+	{
+		count++;
+	}
+	if(!char3.test($('#6').val()))
+	{
+		alert("Contact No should contain only numbers");		
+		return false;
+	}
+	else
+	{
+		count++;
+	}
+	if($('#7').attr("checked")==true)
+	{
+		return true;
+	}
+	else
+	{
+		alert("Accept Terms and Condition");
+		return false;
+	}
+}
+else
+{
+	alert("You must fill all fields");
+	return false;
+}
+}
